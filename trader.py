e33bd26f4f766fffd67b64701bf27184a53e094d
Tobias <me@tobb.dev>
2025-07-17 10:36:08 +0200

    Random Forest Classifier

--- trader.py
+++ trader.py
@@ -1,5 +1,9 @@
-import os
 from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetAssetsRequest
+from alpaca.data import StockHistoricalDataClient
+from alpaca.data.timeframe import TimeFrame
+
+import os
 
 ENV = os.getenv("ENV", "development")
 
@@ -7,10 +11,26 @@ if ENV == "development":
     from dotenv import load_dotenv
     load_dotenv()
 
+# Init alpaca client
+
 API_KEY = os.getenv("ALPACA_API_KEY")
 SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
-client = TradingClient(API_KEY, SECRET_KEY, paper=True)
-account = client.get_account()
+trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+account = trading_client.get_account()
 
-print(f"Account ID: {account.id}")
+# Check traded symblol
+
+TRADED_SYMBOL = "AAPL"
+
+asset = trading_client.get_asset(TRADED_SYMBOL)
+
+if not asset.tradable:
+    raise ValueError(f"{TRADED_SYMBOL} isn't tradable.")
+
+
+# Get historical data
+
+stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+
+# Model
